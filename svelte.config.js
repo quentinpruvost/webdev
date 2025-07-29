82645f430c9d51089013d8cf8e4051258c65583e
Quentin Pruvost <quentinbar26@gmail.com>
2025-07-29 12:02:18 +0200

    chore: Switch to adapter-auto for Vercel deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -7,18 +7,8 @@ const config = {
 	preprocess: [vitePreprocess()],
 
 	kit: {
-		adapter: adapter({
-			// pages: 'build', (valeur par défaut)
-			// assets: 'build', (valeur par défaut)
-			
-			// ON AJOUTE CETTE LIGNE
-			fallback: '404.html'
-		}),
-
-		// ON AJOUTE CETTE SECTION pour pré-rendre toutes les pages
-		prerender: {
-			entries: ['*']
-		}
+		// On utilise simplement l'adaptateur automatique
+		adapter: adapter()
 	}
 };
 
