86732df61c8a2daedeb2feae789d8eb0aea6417a
Quentin Pruvost <quentinbar26@gmail.com>
2025-07-29 11:56:33 +0200

    feat: Configure adapter-static for dynamic routes

--- svelte.config.js
+++ svelte.config.js
@@ -3,12 +3,22 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: ['.svelte'], // On ne gère plus les .md
-	preprocess: [
-		vitePreprocess()
-	],
+	extensions: ['.svelte'],
+	preprocess: [vitePreprocess()],
+
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			// pages: 'build', (valeur par défaut)
+			// assets: 'build', (valeur par défaut)
+			
+			// ON AJOUTE CETTE LIGNE
+			fallback: '404.html'
+		}),
+
+		// ON AJOUTE CETTE SECTION pour pré-rendre toutes les pages
+		prerender: {
+			entries: ['*']
+		}
 	}
 };
 
